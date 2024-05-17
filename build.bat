20c83ecf941646a0c21de43b7ca01cc1141e5a7e
Add Gaming <redthek-e.elsword@web.de>
2024-05-17 22:15:38 +0200

    removed bats

--- build.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd src
-echo --------- MSVC ------------ 
-cl /Wall main.c rstd\\dynamic_array.c lexer.c
-cd ..
-echo --------------------------- 
-copy src\main.exe main.exe
-del src\main.exe
--- test.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-cd src
-echo --------- MSVC ------------- 
-cl /Wall tests.c rstd\\testing.c rstd\\dynamic_array.c lexer.c
-echo --------------------- 
-tests.exe 2> nul :: redirecting stderr to null, since errors are exprected
-del tests.exe
-cd ..
-
